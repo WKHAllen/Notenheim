c7b37dc631f31ac1a91d1ecb78a10eda8bf14276
WKHAllen <wkhallen@gmail.com>
2020-09-02 20:06:53 -0400

    Initialize database

--- src/dbinit.go
+++ src/dbinit.go
@@ -14,45 +14,65 @@ func panicExec(dbm *db.Manager, sql string, params ...interface{}) {
 
 // InitDB creates and, in some cases, populates tables in the database
 func InitDB(dbm *db.Manager) {
-	// Person table
-	panicExec(dbm, `DROP TABLE IF EXISTS person;`)
+	// AppUser table
 	panicExec(dbm, `
-		CREATE TABLE IF NOT EXISTS person (
-			id        SERIAL PRIMARY KEY NOT NULL,
-			firstname VARCHAR(255)       NOT NULL,
-			lastname  VARCHAR(255)       NOT NULL
+		CREATE TABLE IF NOT EXISTS AppUser (
+			id            CHAR(4)      NOT NULL,
+			email         VARCHAR(63)  NOT NULL,
+			password      VARCHAR(127) NOT NULL,
+			verified      BOOLEAN      NOT NULL,
+			joinTimestamp INT          NOT NULL
 		);
 	`)
-	people := []struct{
-		first string
-		last  string
-	}{
-		{first: "Jonas",  last: "Kahnwald"},
-		{first: "Martha", last: "Nielsen"},
-		{first: "Hanno",  last: "Tauber"},
-	}
-	for _, person := range people {
-		panicExec(dbm, `INSERT INTO person (firstname, lastname) VALUES (?, ?);`, person.first, person.last)
-	}
 
-	// Message table
-	panicExec(dbm, `DROP TABLE IF EXISTS quote;`)
+	// List table
 	panicExec(dbm, `
-		CREATE TABLE IF NOT EXISTS quote (
-			id   SERIAL PRIMARY KEY NOT NULL,
-			text VARCHAR(1024)      NOT NULL
+		CREATE TABLE IF NOT EXISTS List (
+			id              CHAR(4)      NOT NULL,
+			userID          CHAR(4)      NOT NULL,
+			title           VARCHAR(255) NOT NULL,
+			createTimestamp INT          NOT NULL,
+			updateTimestamp INT          NOT NULL
 		);
 	`)
-	quotes := []string{
-		"What we know is a drop. What we don't know is an ocean.",
-		"We're not free in what we do because we're not free in what we want.",
-		"Yesterday, today and tomorrow are not consecutive, they are connected in a never-ending circle.",
-		"A man lives three lives. The first one ends with the loss of naivety, the second, with the loss of innocence and the third with the loss of life itself.",
-		"Only when we’ve freed ourselves of emotion can we be truly free. Only when you’re willing to sacrifice what you hold dearest.",
-		"The beginning is the end, and the end is the beginning.",
-		"We all face the same end. Those above have forgotten us. They do not judge us. In death I am alone, and my only judge is me.",
-	}
-	for _, quote := range quotes {
-		panicExec(dbm, `INSERT INTO quote (text) VALUES (?);`, quote)
-	}
+
+	// ListItem table
+	panicExec(dbm, `
+		CREATE TABLE IF NOT EXISTS ListItem (
+			id              CHAR(4)       NOT NULL,
+			listID          CHAR(4)       NOT NULL,
+			content         VARCHAR(1023) NOT NULL,
+			position        INT           NOT NULL,
+			checked         BOOLEAN       NOT NULL,
+			createTimestamp INT           NOT NULL,
+			updateTimestamp INT           NOT NULL
+		);
+	`)
+
+	// Session table
+	panicExec(dbm, `
+		CREATE TABLE IF NOT EXISTS Session (
+			id              CHAR(8) NOT NULL,
+			userID          CHAR(4) NOT NULL,
+			createTimestamp INT     NOT NULL
+		);
+	`)
+
+	// Verify table
+	panicExec(dbm, `
+		CREATE TABLE IF NOT EXISTS Verify (
+			id              CHAR(8)     NOT NULL,
+			email           VARCHAR(63) NOT NULL,
+			createTimestamp INT         NOT NULL
+		);
+	`)
+
+	// PasswordReset table
+	panicExec(dbm, `
+		CREATE TABLE IF NOT EXISTS PasswordReset (
+			id              CHAR(8)     NOT NULL,
+			email           VARCHAR(63) NOT NULL,
+			createTimestamp INT         NOT NULL
+		);
+	`)
 }
