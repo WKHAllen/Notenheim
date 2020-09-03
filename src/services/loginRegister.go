967cd16fd21164ef23be99dfd41ebc9bed6c888b
WKHAllen <wkhallen@gmail.com>
2020-09-03 14:30:23 -0400

    Comment service functions

--- src/services/loginRegister.go
+++ src/services/loginRegister.go
@@ -11,18 +11,21 @@ import (
 func Register(email string, password string) error {
 	var id string
 
+	// Confirm that the email address has not already been used
 	sql := "SELECT id FROM AppUser WHERE email = ?;"
 	err := dbm.QueryRow(sql, email).Scan(&id)
 	if err == nil {
 		return fmt.Errorf("Email address has already been registered")
 	}
 
+	// Hash the password
 	hashed, err := app.HashPassword(password)
 	if err != nil {
 		fmt.Printf("Unexpected error: %v\n", err)
 		return fmt.Errorf("An unexpected error occurred")
 	}
 
+	// Add the new user to the database
 	sql = `
 		INSERT INTO AppUser
 			(id, email, password, verified, joinTimestamp)
@@ -43,16 +46,19 @@ func Login(email string, password string) (string, error) {
 	var userID string
 	var hashed string
 
+	// Verify the provided email exists
 	sql := "SELECT id, password FROM AppUser WHERE email = ? AND verified = TRUE;"
 	err := dbm.QueryRow(sql, email).Scan(&userID, &hashed)
 	if err != nil {
 		return "", fmt.Errorf("Invalid login")
 	}
 
+	// Verify the provided password
 	if !app.ComparePassword(password, hashed) {
 		return "", fmt.Errorf("Invalid login")
 	}
 
+	// Create a new session for the user
 	sessionID := helper.UniqueBase64ID(8, dbm, "Session", "id")
 	sql = `
 		INSERT INTO Session
@@ -65,6 +71,7 @@ func Login(email string, password string) (string, error) {
 		return "", fmt.Errorf("An unexpected error occurred")
 	}
 
+	// Delete all but the user's four most recent sessions
 	sql = `
 		DELETE FROM Session
 		WHERE userID = ? AND id NOT IN (
@@ -84,6 +91,7 @@ func Login(email string, password string) (string, error) {
 
 // Logout logs a user out
 func Logout(sessionID string) error {
+	// Delete the provided session
 	sql := "DELETE FROM Session WHERE id = ?;"
 	err := dbm.Execute(sql, sessionID)
 	if err != nil {
--- src/services/passwordReset.go
+++ src/services/passwordReset.go
@@ -11,18 +11,21 @@ import (
 func RequestPasswordReset(email string) error {
 	var id string
 
+	// Confirm that the email address exists
 	sql := "SELECT id FROM AppUser WHERE email = ? AND verified = TRUE;"
 	err := dbm.QueryRow(sql, email).Scan(&id)
 	if err != nil {
 		return fmt.Errorf("Email address not registered")
 	}
 
+	// Check that no password reset has already been requested
 	sql = "SELECT id FROM PasswordReset WHERE email = ?;"
 	err = dbm.QueryRow(sql, email).Scan(&id)
 	if err == nil {
 		return fmt.Errorf("Password reset has already been requested")
 	}
 
+	// Create password reset request
 	sql = `
 		INSERT INTO PasswordReset
 			(id, email, createTimestamp)
@@ -42,6 +45,7 @@ func RequestPasswordReset(email string) error {
 func ValidPasswordResetID(resetID string) bool {
 	var email string
 
+	// Check if password reset ID exists
 	sql := "SELECT email FROM PasswordReset WHERE id = ?;"
 	err := dbm.QueryRow(sql, resetID).Scan(&email)
 	return err == nil
@@ -51,12 +55,14 @@ func ValidPasswordResetID(resetID string) bool {
 func ResetPassword(resetID string, newPassword string) error {
 	var email string
 
+	// Confirm that the password reset ID exists
 	sql := "SELECT email FROM PasswordReset WHERE id = ?;"
 	err := dbm.QueryRow(sql, resetID).Scan(&email)
 	if err != nil {
 		return fmt.Errorf("Invalid password reset ID")
 	}
 
+	// Delete the password reset ID
 	sql = "DELETE FROM PasswordReset WHERE id = ?;"
 	err = dbm.Execute(sql, resetID)
 	if err != nil {
@@ -64,12 +70,14 @@ func ResetPassword(resetID string, newPassword string) error {
 		return fmt.Errorf("An unexpected error occurred")
 	}
 
+	// Hash the new password
 	hashed, err := app.HashPassword(newPassword)
 	if err != nil {
 		fmt.Printf("Unexpected error: %v\n", err)
 		return fmt.Errorf("An unexpected error occurred")
 	}
 
+	// Change the password
 	sql = "UPDATE AppUser SET password = ? WHERE email = ?;"
 	err = dbm.Execute(sql, hashed, email)
 	if err != nil {
