c76008acbb9d6558a78cfebf6ecf77fecd47ee3c
WKHAllen <wkhallen@gmail.com>
2020-09-07 16:03:49 -0400

    Handle registration success event

--- app/src/components/Register.tsx
+++ app/src/components/Register.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../css/Register.css';
 import { requestAPIForm } from '../requestAPI';
-import { showAPIError } from '../apiError';
+import { showAPIError, hideAPIError } from '../apiError';
 
 interface RegisterState {
-	formGood: boolean
+	formGood: boolean,
+	submitClicked: boolean,
+	registerSuccess: boolean
 }
 
 export default class Register extends React.Component<any, RegisterState> {
@@ -12,42 +14,63 @@ export default class Register extends React.Component<any, RegisterState> {
 		super(props);
 
 		this.state = {
-			formGood: false
+			formGood: false,
+			submitClicked: false,
+			registerSuccess: false
 		};
 	}
 
 	public render() {
-		return (
-			<div className="Register">
-				<h1>Register</h1>
-				<form onSubmit={event => { this.register(event); return false; }}>
-					<div className="form-group">
-						<label htmlFor="email">Email</label>
-						<input type="email" className="form-control" id="email" name="email" onChange={() => this.checkPasswords()} />
-					</div>
-					<div className="form-group">
-						<label htmlFor="password">Password</label>
-						<input type="password" className="form-control" id="password" name="password" onChange={() => this.checkPasswords()} />
-					</div>
-					<div className="form-group">
-						<label htmlFor="confirm-password">Confirm password</label>
-						<input type="password" className="form-control" id="confirm-password" name="confirm-password" onChange={() => this.checkPasswords()} />
-					</div>
-					<button type="submit" className="btn btn-primary btn-pink" id="register-button" disabled={!this.state.formGood}>Register</button>
-				</form>
-			</div>
-		);
+		if (!this.state.registerSuccess) {
+			return (
+				<div className="Register">
+					<h1>Register</h1>
+					<form onSubmit={event => { this.register(event); return false; }}>
+						<div className="form-group">
+							<label htmlFor="email">Email</label>
+							<input type="email" className="form-control" id="email" name="email" onChange={() => this.checkPasswords()} />
+						</div>
+						<div className="form-group">
+							<label htmlFor="password">Password</label>
+							<input type="password" className="form-control" id="password" name="password" onChange={() => this.checkPasswords()} />
+						</div>
+						<div className="form-group">
+							<label htmlFor="confirm-password">Confirm password</label>
+							<input type="password" className="form-control" id="confirm-password" name="confirm-password" onChange={() => this.checkPasswords()} />
+						</div>
+						<button type="submit" className="btn btn-primary btn-pink" id="register-button" disabled={!this.state.formGood || this.state.submitClicked}>Register</button>
+					</form>
+				</div>
+			);
+		} else {
+			return (
+				<div className="Register">
+					<h1>Register</h1>
+					<p>Congratulations! You have registered for Notenheim. In the next few minutes you will receive an email containing a link that will confirm your registration. All you need to do is click the link in the email and your account will be ready! Please do so soon, as the link will expire after one hour.</p>
+				</div>
+			)
+		}
 	}
 
 	private async register(e: React.FormEvent<HTMLFormElement>): Promise<void> {
 		e.preventDefault();
 
+		this.setState({
+			submitClicked: true
+		});
+
 		const formData = new FormData(e.currentTarget);
 		const res = await requestAPIForm('/register', formData, ['email', 'password']);
 
 		if (res.error === null) {
-			// show success
+			hideAPIError();
+			this.setState({
+				registerSuccess: true
+			});
 		} else {
+			this.setState({
+				submitClicked: false
+			});
 			showAPIError(res.error);
 		}
 	}
