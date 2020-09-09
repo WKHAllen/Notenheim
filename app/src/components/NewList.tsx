3c8b002fbc2b3e8b621350d82ce497c0dfc0c6ef
WKHAllen <wkhallen@gmail.com>
2020-09-09 13:10:54 -0400

    Build new list page

--- app/src/components/NewList.tsx
+++ app/src/components/NewList.tsx
@@ -1,12 +1,78 @@
 import React from 'react';
 import '../css/NewList.css';
+import { requestAPI } from '../requestAPI';
+import { showAPIError, hideAPIError } from '../apiError';
+import { getCookie } from '../cookie';
 
-export default class NewList extends React.Component {
-	render() {
+interface NewListState {
+	formGood: boolean,
+	submitClicked: boolean
+}
+
+export default class NewList extends React.Component<any, NewListState> {
+	constructor(props: any) {
+		super(props);
+
+		this.state = {
+			formGood: false,
+			submitClicked: false
+		};
+	}
+
+	public componentWillMount() {
+		if (getCookie('loggedIn') !== 'true') {
+			this.props.history.push('/login?after=/new');
+		}
+	}
+
+	public render() {
 		return (
 			<div className="NewList">
-
+				<h1>New List</h1>
+				<form onSubmit={event => { this.createNewList(event); return false; }} className="mb-3">
+					<div className="form-group">
+						<label htmlFor="list-name">List name</label>
+						<input type="text" className="form-control" id="list-name" name="list-name" maxLength={255} onChange={() => this.checkForm()} />
+						<small className="form-text text-muted">You can change this later.</small>
+					</div>
+					<button type="submit" className="btn btn-primary btn-pink" disabled={!this.state.formGood || this.state.submitClicked}>Create list</button>
+				</form>
 			</div>
 		);
+	}
+
+	private async createNewList(e: React.FormEvent<HTMLFormElement>): Promise<void> {
+		e.preventDefault();
+
+		this.setState({
+			submitClicked: true
+		});
+
+		const formData = new FormData(e.currentTarget);
+		const res = await requestAPI('/newList', {
+			title: formData.get('list-name')
+		});
+
+		this.setState({
+			submitClicked: false
+		});
+
+		if (res.error === null) {
+			hideAPIError();
+			this.props.history.push(`/list/${res.listID}`);
+		} else {
+			this.setState({
+				submitClicked: false
+			});
+			showAPIError(res.error);
+		}
+	}
+
+	private async checkForm(): Promise<void> {
+		const listName = (document.getElementById('list-name') as HTMLInputElement).value;
+
+		this.setState({
+			formGood: listName.length >= 1
+		});
 	}
 }
