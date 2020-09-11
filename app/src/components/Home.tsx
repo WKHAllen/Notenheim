52e60f87654360d6f3b97d57f1d881a99e71d864
WKHAllen <wkhallen@gmail.com>
2020-09-10 22:51:47 -0400

    Add new list button to home page

--- app/src/components/Home.tsx
+++ app/src/components/Home.tsx
@@ -53,9 +53,14 @@ export default class Home extends React.Component<any, HomeState> {
 						<ul>
 							<li>
 								<h2>Your lists</h2>
-								<button type="button" className="btn btn-primary btn-pink btn-icon" onClick={() => this.getLists()} disabled={this.state.refreshClicked}>
-									<i className="fas fa-sync-alt" />
-								</button>
+								<div className="Home-List-Buttons">
+									<button type="button" className="btn btn-primary btn-pink btn-icon" onClick={() => this.props.history.push('/new')}>
+										<i className="fas fa-plus" />
+									</button>
+									<button type="button" className="btn btn-primary btn-pink btn-icon" onClick={() => this.getLists()} disabled={this.state.refreshClicked}>
+										<i className="fas fa-sync-alt" />
+									</button>
+								</div>
 							</li>
 							{this.state.lists.map(item => 
 								<li key={item.listID}>
